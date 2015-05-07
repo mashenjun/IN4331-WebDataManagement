970d75be933a7878acdb571ea52e30337190e3f1
mashenjun <mashenjun@sina.cn>
2015-05-07 22:26:34 +0200

    complete the Getting Start

--- project/mysite/mysite/views.py
+++ project/mysite/mysite/views.py
@@ -114,10 +114,9 @@ def postdata(Num):
 
 def sendrequest(request):
     import requests
-
     par_list=[]
-    body="//"
-    mark="-"
+    body=""
+    mark="+"
     datastring=""
     for i in range(1,7,1):
         a='par'+str(i)
@@ -127,16 +126,55 @@ def sendrequest(request):
             b.pop()
             #print (b)
         par_list.append(b)
-    #print (par_list[0]+" "+par_list[1])
+    #print (len(par_list[3]))
     url="http://localhost:8080/exist/rest/db/movies/movies.xml?_query="
     # body=body+'movie[title=\"'+par_list[0]+'\"]/title'
     if par_list[0]!="":
         body=body+'title=\"'+par_list[0]+'\"'
-    elif par_list[1]!="":
-        body = body+'movie[genre=\"'+par_list[1]+'\"]/title'
+    if par_list[0]=="":
+        if par_list[1]!="":
+            if body=="" :
+                body = body+'genre=\"'+par_list[1]+'\"'
+            else :
+                body = body+' and '+'genre=\"'+par_list[1]+'\"'
+        if par_list[2]!="":
+            lastname= par_list[2].split(' ')[0]
+            firstname= par_list[2].split(' ')[1]
+            if body=="" :
+                body = body+'director/last_name=\"'+lastname+'\"'+' and '+'director/first_name=\"'+firstname+'\"'
+            else :
+                body = body+' and '+'director/last_name=\"'+lastname+'\"'+' and '+'director/first_name=\"'+firstname+'\"'
+        if par_list[3]!="":
+            name=par_list[3]
+            lastname='';
+            firstname='';
+            for x in range(0, len(name)):
+                print(len(name[x].split(' ')))
+                if len(name[x].split(' '))==2:
+                    print ('<2>')
+                    lastname= name[x].split(' ')[0]
+                    firstname= name[x].split(' ')[1]
+                elif len(name[x].split(' '))==3:
+                    print ('<3>')
+                    lastname = name[x].split(' ')[0]+" "+name[x].split(' ')[1]
+                    firstname = name[x].split(' ')[2]
+                if body=="":
+                    body = body+'actor[last_name=\"'+lastname+'\"'+' and '+'first_name=\"'+firstname+'\"'+']'
+                else :
+                    body = body+' and '+'actor[last_name=\"'+lastname+'\"'+' and '+'first_name=\"'+firstname+'\"'+']'
+        if par_list[4]!="":
+            if body=="" :
+                body = body+'year=\"'+par_list[4]+'\"'
+            else :
+                body = body+' and '+'year=\"'+par_list[4]+'\"'
+        if par_list[5]!="":
+            if body=="" :
+                body = body+'summary[contains(.,\"'+par_list[5]+'\"'+')]'
+            else :
+                body = body+' and '+'summary[contains(.,\"'+par_list[5]+'\"'+')]'
 
 
-    request_content= url+body
+    request_content= url+'//movie['+body+']/title'
     print (request_content)
     result=requests.get(request_content)
     #print (result.content)
